b511df15335a3dac83b24978b606d36c25e1d95f
sanni <aryansingh7371@gmail.com>
2025-03-18 16:26:46 +0530

    made some chnages

--- backend/src/index.js
+++ backend/src/index.js
@@ -46,13 +46,13 @@ cloudinary.config({
 });
 
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"../frontent/dist")))
-
-    app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname,"../frontent","dist","index.html"))
-    })
-}
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+  }
 
 
 // here we are running or server
